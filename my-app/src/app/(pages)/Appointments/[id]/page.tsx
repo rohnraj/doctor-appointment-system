16f877b5abeccfa13bd72bcafaa4c2ec5331fba0
rohan <rohan.raj@tothenew.com>
2025-03-26 22:30:56 +0530

    data from slots page collected and wrapped into the url as a query parameter

--- my-app/src/app/(pages)/Appointments/[id]/page.tsx
+++ my-app/src/app/(pages)/Appointments/[id]/page.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import React, { useRef, useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import styles from './page.module.css'
 import Navbar from '@/app/components/navbar/Navbar'
 import Button from '@/app/components/button/Button'
@@ -30,6 +31,8 @@ const page =()=> {
 
     let {id}=useParams<{ id: string }>()
 
+    const router=useRouter()
+
     let [isOpen, setIsOpen] = useState(false)
     let [pisOpen, setpIsOpen] = useState(false)
     // let [addressData, setaddressData]=useState<string | null>(null)
@@ -38,6 +41,8 @@ const page =()=> {
 
     const [selectedDate, setSelectedDate] = useState<string> ('')
     const [fetchDate, setfetchDate] = useState<string[]>([])
+    const [typeofConsult, settypeofConsult] = useState('online')
+    const [selectedTImeSlot, setselectedTImeSlot]=useState('')
 
     
     useEffect(()=>{ 
@@ -72,8 +77,29 @@ const page =()=> {
 
     console.log(selectedDate)
       
-    
+    function bookSlot(time: string){
+        console.log('Time slot'+time)
+        setselectedTImeSlot(time)
+    }
 
+
+    const AllSlotData=new URLSearchParams({
+        date:selectedDate,
+        time:selectedTImeSlot,
+        typeofConsult:typeofConsult,
+        doctorId:id,
+        name:doctorData?.name || '',
+        specialty:doctorData?.specialty || '',
+        location:doctorData?.location || '',
+    }).toString()
+
+    function handleNext(){
+        const encodedURL = `/bookingConfirmatinon?${AllSlotData}`;
+        // create folder in appoitments for this
+        console.log(encodedURL);
+
+        router.push(`/Appointments/${encodedURL}`)        
+    }
   return (  
     <>
 
@@ -98,19 +124,18 @@ const page =()=> {
                             <Button text={'Book Appointment'} onClick={()=>{}} type={'submit'} variant={'smallcardButtonGreen'}/>
                         </div>
                         <div className={styles.booking}>
-                            <button>Book Video Consult</button>
-                            <button>Book Hospital Visit</button>
+                            <button onClick={()=>{
+                                settypeofConsult('online')
+                            }}>Book Video Consult</button>
+                            <button onClick={()=>{
+                                settypeofConsult('offline')
+                            }}>Book Hospital Visit</button>
                         </div>
                         <div className={styles.address}>
                             <div className={styles.select} onClick={()=>setIsOpen(!isOpen)}>
                                 <p className={pisOpen ? styles.showpTag : styles.notshowpTag}>{doctorData?.location}</p>
                                 <i className="fa-solid fa-caret-down"></i>
                                 <ul className={isOpen==true ? styles.show_select_options : styles.hide_select_options} onClick={()=>setpIsOpen(true)}>
-                                    {/* {options.map((option, index) => (
-                                        <li key={index} onClick={() => setSelectedOption(option)}>
-                                            {option}
-                                        </li>
-                                    ))} */}
                                     <li>{doctorData?.location}</li>
                                 </ul>
                             </div>
@@ -131,14 +156,14 @@ const page =()=> {
                             </div>
                             <div>
                             {/* && fetchDate?.includes(selectedDate) */}
-                                <span><Button text={'9:00 AM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'}  disabled={doctorData?.available_times.includes('09:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'9:30 AM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('09:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'10:00 AM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('10:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'10:30 AM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('10:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'11:00 AM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('11:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'11:30 AM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('11:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'12:00 AM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('12:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'12:30 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('12:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'9:00 AM'} onClick={()=>bookSlot('9:00 AM')} type={'submit'} variant={'slotGreenBtn'}  disabled={doctorData?.available_times.includes('09:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'9:30 AM'} onClick={()=>bookSlot('9:30 AM')}  type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('09:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'10:00 AM'} onClick={()=>bookSlot('10:00 AM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('10:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'10:30 AM'} onClick={()=>bookSlot('10:30 AM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('10:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'11:00 AM'} onClick={()=>bookSlot('11:00 AM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('11:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'11:30 AM'} onClick={()=>bookSlot('11:30 AM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('11:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'12:00 AM'} onClick={()=>bookSlot('12:00 AM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('12:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'12:30 PM'} onClick={()=>bookSlot('12:30 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('12:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
                             </div>
                         </div>
                         <div className={styles.slotTwo}>
@@ -151,18 +176,20 @@ const page =()=> {
                             </div>
                             <hr/>
                             <div>
-                                <span><Button text={'1:00 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('13:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'1:30 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('13:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'2:00 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('14:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'2:30 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('14:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'3:00 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('15:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'3:30 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('15:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'4:00 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('16:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
-                                <span><Button text={'4:30 PM'} onClick={()=>{}} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('16:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'1:00 PM'} onClick={()=>bookSlot('1:00 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('13:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'1:30 PM'} onClick={()=>bookSlot('1:30 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('13:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'2:00 PM'} onClick={()=>bookSlot('2:00 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('14:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'2:30 PM'} onClick={()=>bookSlot('2:30 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('14:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'3:00 PM'} onClick={()=>bookSlot('3:00 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('15:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'3:30 PM'} onClick={()=>bookSlot('3:30 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('15:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'4:00 PM'} onClick={()=>bookSlot('4:00 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('16:00:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
+                                <span><Button text={'4:30 PM'} onClick={()=>bookSlot('4:30 PM')} type={'submit'} variant={'slotGreenBtn'} disabled={doctorData?.available_times.includes('16:30:00') && fetchDate?.includes(selectedDate) ? false : true}/></span>
                             </div>
                         </div>
                     </article>
-                    <Button text={'Next'} onClick={()=>{}} type={'submit'} variant={'largeGreenBtn'}/>
+                    <Button text={'Next'} onClick={()=>{
+                        handleNext()
+                    }} type={'submit'} variant={'largeGreenBtn'}/>
                 </div>
             </div>
         </main>
