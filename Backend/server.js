67afe0cda2fef01dac16cc279504dc1963f9c9cc
HrxTiger <jhaharsh451@gmail.com>
2025-09-14 22:59:44 +0530

    Update CORS configuration to include production frontend URL

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000" ,  'http://localhost:3002', 'http://localhost:3001'], // Allow requests from frontend
+    origin: ["https://doctor-appointment-system-q8td.vercel.app/", "http://localhost:3000" ,  'http://localhost:3002', 'http://localhost:3001'], // Allow requests from frontend
     credentials: true, // Allow cookies and authentication headers
   })
 );
